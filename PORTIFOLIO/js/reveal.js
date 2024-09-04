ff6191137911ec7427d76c804e40d3aca06f3413
LuanHSAlves-Dev <luansantana.dev@gmail.com>
2024-09-04 20:01:39 -0300

    Add files via upload

---
+++ PORTIFOLIO/js/reveal.js
@@ -0,0 +1,200 @@
+window.reveal = ScrollReveal({reset:true})
+
+reveal.reveal('.e-inicio',
+{
+    duration: 2000,
+    distance: '100px'
+})
+
+reveal.reveal('.e-fundo',
+{
+    duration: 2000,
+    distance: '100px',
+    origin: 'bottom',
+    delay: 300
+
+})
+
+reveal.reveal('.e-link',
+{
+    duration: 2000,
+    distance: '100px',
+    origin: 'left',
+    delay: 100
+
+})
+
+reveal.reveal('.e-insta',
+{
+    duration: 2000,
+    distance: '100px',
+    origin: 'left',
+    delay: 200
+
+})
+
+reveal.reveal('.e-github',
+{
+    duration: 2000,
+    distance: '100px',
+    origin: 'left',
+    delay: 300
+
+})
+
+reveal.reveal('.e-email',
+{
+    duration: 2000,
+    distance: '100px',
+    origin: 'left',
+    delay: 400
+
+})
+
+reveal.reveal('.e-social',
+{
+    duration: 2000,
+    distance: '100px',
+    origin: 'left',
+    delay: 50
+
+})
+
+
+
+
+
+
+reveal.reveal('.e-skills',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'top',
+    delay: 100,
+    opacity: 0
+
+})
+
+reveal.reveal('.e-py',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'bottom',
+    delay: 100,
+    opacity: 0
+
+})
+
+reveal.reveal('.e-sql',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'bottom',
+    delay: 200,
+    opacity: 0
+
+})
+
+reveal.reveal('.e-bi',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'bottom',
+    delay: 300,
+    opacity: 0
+
+})
+
+reveal.reveal('.e-aws',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'bottom',
+    delay: 400,
+    opacity: 0
+
+})
+
+reveal.reveal('.e-ex',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'bottom',
+    delay: 500,
+    opacity: 0
+
+})
+
+reveal.reveal('.e-jupy',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'bottom',
+    delay: 600,
+    opacity: 0
+
+})
+
+reveal.reveal('.e-git',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'bottom',
+    delay: 700,
+    opacity: 0
+
+})
+
+reveal.reveal('.e-js',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'bottom',
+    delay: 900,
+    opacity: 0
+
+})
+
+
+
+
+reveal.reveal('.e-pro',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'top',
+    delay: 100,
+    opacity: 0
+
+})
+
+reveal.reveal('.grade2',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'bottom',
+    delay: 100,
+    opacity: 0
+
+})
+
+reveal.reveal('.e-sobre',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'top',
+    delay: 100,
+    opacity: 0
+
+})
+
+reveal.reveal('.time',
+{
+    duration: 2000,
+    distance: '150px',
+    origin: 'bottom',
+    delay: 100,
+    opacity: 0
+
+})
+
